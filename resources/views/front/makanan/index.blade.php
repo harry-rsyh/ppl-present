1f1a457f173e0e75c47ffdc08150a4ef4cf248bb
rahmanmuh706 <rahmanmuh706@gmail.com>
2019-04-03 12:53:43 +0700

    cobaa

--- resources/views/front/makanan/index.blade.php
+++ resources/views/front/makanan/index.blade.php
@@ -23,6 +23,27 @@
     </div>
     <!-- Kotak -->
 
+
+<!-- Kotak -->
+<div class="col-lg-3 col-md-4" role="button" data-toggle="modal" data-target="#modalGaleri">
+        <div class="card shadow mb-4 py-2 border-left-primary">
+            <img src="img/menu-image1.jpg" class="img-responsive img-fluid p-2">
+            <div class="card-body">
+                <div class="menu-info">
+                    <div class="menu-item">
+                        <h3>Udang Kecap</h3>
+                        <p>Udang / Bumbu Kecap</p>
+                    </div>
+                    <div class="menu-price">
+                    <span>Rp.25K</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Kotak -->
+
+
     <!-- Kotak -->
     <div class="col-lg-3 col-md-4" role="button" data-toggle="modal" data-target="#modalGaleri">
         <div class="card shadow mb-4 py-2 border-left-primary">
