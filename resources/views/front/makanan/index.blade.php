e2f5fc79723b66b302a2f675e6e0f6b4e4765e7b
rahmanmuh706 <rahmanmuh706@gmail.com>
2019-04-03 08:36:58 +0700

    penambahan gambar dan menu minumam dessert

--- resources/views/front/makanan/index.blade.php
+++ resources/views/front/makanan/index.blade.php
@@ -42,8 +42,82 @@
     </div>
     <!-- Kotak -->
 
-
+    <!-- Kotak -->
+    <div class="col-lg-3 col-md-4" role="button" data-toggle="modal" data-target="#modalGaleri">
+        <div class="card mb-4 py-2 border-left-primary">
+            <img src="img/menu-image3.jpg" class="img-responsive img-fluid p-2">
+            <div class="card-body">
+                <div class="menu-info">
+                    <div class="menu-item">
+                        <h3>Mie Goreng</h3>
+                        <p>Jumbo / Pedas</p>
+                    </div>
+                    <div class="menu-price">
+                    <span>Rp.18K</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Kotak -->
     
+    <!-- Kotak -->
+    <div class="col-lg-3 col-md-4" role="button" data-toggle="modal" data-target="#modalGaleri">
+        <div class="card mb-4 py-2 border-left-primary">
+            <img src="img/menu-image4.jpg" class="img-responsive img-fluid p-2">
+            <div class="card-body">
+                <div class="menu-info">
+                    <div class="menu-item">
+                        <h3>Nasgor Telor</h3>
+                        <p>Biasa / Jumbo</p>
+                    </div>
+                    <div class="menu-price">
+                    <span>Rp.20K</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Kotak -->
+
+    <!-- Kotak -->
+    <div class="col-lg-3 col-md-4" role="button" data-toggle="modal" data-target="#modalGaleri">
+        <div class="card mb-4 py-2 border-left-primary">
+            <img src="img/menu-image6.jpg" class="img-responsive img-fluid p-2">
+            <div class="card-body">
+                <div class="menu-info">
+                    <div class="menu-item">
+                        <h3>Ketoprak</h3>
+                        <p>Non Sayur / Sayur</p>
+                    </div>
+                    <div class="menu-price">
+                    <span>Rp.15K</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Kotak -->
+
+    <!-- Kotak -->
+    <div class="col-lg-3 col-md-4" role="button" data-toggle="modal" data-target="#modalGaleri">
+        <div class="card mb-4 py-2 border-left-primary">
+            <img src="img/menu-image5.jpg" class="img-responsive img-fluid p-2">
+            <div class="card-body">
+                <div class="menu-info">
+                    <div class="menu-item">
+                        <h3>Soto Kudus</h3>
+                        <p>Besar / Kecil</p>
+                    </div>
+                    <div class="menu-price">
+                    <span>Rp.14K</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Kotak -->
+
 </div>
 @endsection
 
