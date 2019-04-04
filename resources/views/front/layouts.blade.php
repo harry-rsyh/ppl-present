af462f95c697383674baea595ca02c6e3b629a40
rahmanmuh706 <rahmanmuh706@gmail.com>
2019-04-05 01:23:12 +0700

    update profil

--- resources/views/front/layouts.blade.php
+++ resources/views/front/layouts.blade.php
@@ -323,6 +323,7 @@
   </a>
 
   <!-- Logout Modal-->
+  
   <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
