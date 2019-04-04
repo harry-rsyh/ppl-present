1bc241eb4e2cf4f4301200fb0cf24aa30bf413b1
rahmanmuh706 <rahmanmuh706@gmail.com>
2019-04-05 01:44:22 +0700

    a

---
+++ resources/views/kasir/transaksi/action-button.blade.php
@@ -0,0 +1,4 @@
+<div class="button-list" data-table-target="table-galeri" >
+    <!-- Custom width modal -->
+    <button type="button" class="btn btn-sm btn-icon waves-effect waves-light btn-warning"><i class="mdi mdi-magnify"></i></button>
+</div>
