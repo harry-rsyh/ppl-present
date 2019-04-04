62119e8926c552fbb5024b6b9bd81440f6eb5ff8
harryrahmansyah97 <harryrahmansyah97@gmail.com>
2019-04-05 01:18:12 +0700

    Menu kasir

---
+++ resources/views/kasir/pembayaran/action-button.blade.php
@@ -0,0 +1,5 @@
+<div class="button-list" data-table-target="table-galeri" >
+    <button type="button" class="btn btn-sm btn-icon waves-effect waves-light btn-danger"><i class="mdi mdi-close-octagon"></i></button>
+    <button type="button" class="btn btn-sm btn-icon waves-effect waves-light btn-warning" data-toggle="modal" data-target="#infoModal"><i class="mdi mdi-magnify"></i></button>
+    <button type="button" class="btn btn-sm btn-icon waves-effect waves-light btn-primary"><i class="mdi mdi-check-decagram"></i></button>
+</div>
---
+++ resources/views/kasir/pembayaran/info.blade.php
@@ -0,0 +1,48 @@
+<!-- Logout Modal-->
+<div class="modal fade" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="infoModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Info Pembayaran</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <div class="table-responsive">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataPesanan" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Menu</th>
+                                <th>Jumlah</th>
+                                <th>Harga</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Udang Kecap</td>
+                                <th>3</th>
+                                <td>Rp 75.000</td>
+                            </tr>
+                            <tr>
+                                <td>Ikan Bakar</td>
+                                <th>1</th>
+                                <td>Rp 20.000</td>
+                            </tr>
+                            <tr>
+                                <td>Teh</td>
+                                <th>4</th>
+                                <td>Rp 20.000</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button class="btn btn-primary" type="button">Selesai</button>
+        </div>
+      </div>
+    </div>
+</div>
