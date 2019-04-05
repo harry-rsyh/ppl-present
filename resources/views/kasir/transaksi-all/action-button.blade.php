19cc0a5a3c8e683ea8c559b55efbf0527f7feaeb
harryrahmansyah97 <harryrahmansyah97@gmail.com>
2019-04-05 11:25:04 +0700

    Revert "Menu kasir"
    This reverts commit 62119e8926c552fbb5024b6b9bd81440f6eb5ff8.

--- resources/views/kasir/transaksi-all/action-button.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="button-list" data-table-target="table-galeri" >
-    <!-- Custom width modal -->
-    <button type="button" class="btn btn-sm btn-icon waves-effect waves-light btn-warning"><i class="mdi mdi-magnify"></i></button>
-</div>
